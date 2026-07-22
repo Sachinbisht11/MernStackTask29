11fb83a532c4a20b8de3b8a245b0cd86dedf4e13
Sachin_Bisht <sachinbisht18122004@gmail.com>
2026-07-23 00:33:45 +0530

    Update useFetch.js

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -4,27 +4,34 @@ function useFetch(url) {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-
   useEffect(() => {
+    const controller = new AbortController();
     async function fetchData() {
       try {
         setLoading(true);
-        const response = await fetch(url);
+        const response = await fetch(url ,{
+          signal: controller.signal,
+        });
         if (!response.ok) {
           throw new Error("Failed to fetch");
         }
-        await new Promise(resolve => setTimeout(resolve, 3000));
         const result = await response.json();
         setData(result);
-      } catch (err) {
+      } 
+      catch (err) {
         console.log(JSON.stringify(err.message),"errr")
-        await new Promise(reject => setTimeout(reject, 1000));
-        setError(`Error: ${err.message}`);
-      } finally {
+        if (err.name !== "AbortError") {
+          setError(err.message);
+        }
+      } 
+      finally {
         setLoading(false);
-      };
+      }
     }
-  fetchData();
+    fetchData();
+    return () => {
+      controller.abort();
+    };
   }, [url]);
 
   return { data, loading, error };
